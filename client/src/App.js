e36b3401a6c0c7ddde8702814def05918b4dc927
kas-rabur <raburskikacper@gmail.com>
2025-04-02 11:03:43 +0100

    navbar now only appears on certain pages, excluding login

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import UserDashboard from "./components/UserDashboard";
 import GovernmentDashboard from "./components/GovernmentDashboard";
@@ -8,18 +8,28 @@ import LoginPage from "./components/LoginPage";
 import "./css/App.css"; 
 
 function App() {
+  const location = useLocation();
+  const excludedRoutes = ["/Login"]; 
+
   return (
-    <Router>
-      <Navbar />
+    <div>
+      {!excludedRoutes.includes(location.pathname) && <Navbar />}
       <Routes>
         <Route path="/UserDashboard" element={<UserDashboard />} />
         <Route path="/GovernmentDashboard" element={<GovernmentDashboard />} />
         <Route path="/MerchantDashboard" element={<MerchantDashboard />} />
         <Route path="/Login" element={<LoginPage />} />
-
       </Routes>
+    </div>
+  );
+}
+
+function AppWrapper() {
+  return (
+    <Router>
+      <App />
     </Router>
   );
 }
 
-export default App;
+export default AppWrapper;
