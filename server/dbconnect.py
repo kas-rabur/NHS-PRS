167ac78fb941251c888b151165d78e8e91b9c9ae
kas-rabur <raburskikacper@gmail.com>
2025-05-09 12:59:52 +0100

    trying to connect to db

--- server/dbconnect.py
+++ server/dbconnect.py
@@ -1,14 +1,25 @@
-import pyodbc
-print(pyodbc.drivers())
+from sqlalchemy import create_engine, text
 
+server = "localhost\\KAS"
+database = "NHS-PRS"
+username = "your_username"
+password = "your_password"
+driver = "ODBC Driver 17 for SQL Server"
+
+
+connection_string = (
+    f"mssql+pyodbc://@{server}/{database}"
+    f"?driver={driver.replace(' ', '+')}&trusted_connection=yes"
+)
+
+
+# Create the engine
 try:
-    conn = pyodbc.connect(
-        'DRIVER={ODBC Driver 18 for SQL Server};'
-        'SERVER=ML-RefVm-759941;' 
-        'DATABASE=PRS;'
-        'UID=sa;'
-        'PWD=sa1234'
-    )
-    print("✅ Connection successful")
+    engine = create_engine(connection_string)
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT GETDATE() AS CurrentTime"))
+        for row in result:
+            print(f"✅ Connected! SQL Server time is: {row['CurrentTime']}")
 except Exception as e:
-    print("❌ Connection failed:", e)
+    print("❌ Failed to connect to SQL Server.")
+    print("Error:", e)
